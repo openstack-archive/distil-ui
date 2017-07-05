ab1132cacb1aa384c7b702cfd2e1f48a8af27f95
Amelia Cordwell <ameliacordwell@catalyst.net.nz>
2017-07-05 15:22:21 +1200

    Refactor distil api for distil v2
    * Support displaying balance of credits
    * Fewer API calls
    * Returns data across all regions instead of just the current one
    * Support viewing details for previous invoices
    * Highlight unpaid invoices
    
    Change-Id: Ic0f950948c83e76d760123046e8ffbd12dd632dd

--- distil_ui/content/billing/views.py
+++ distil_ui/content/billing/views.py
@@ -14,103 +14,51 @@
 
 import datetime
 import json
+import logging
 
-from django.template import defaultfilters
-from django.utils.translation import ugettext_lazy as _
-from horizon import exceptions
-from horizon import tables as horizon_tables
-from horizon.utils import csvbase
+from horizon import views
 
-from distil_ui.api import distil
-from distil_ui.content.billing import base
-from distil_ui.content.billing import tables
+from distil_ui.api import distil_v2 as distil
+
+LOG = logging.getLogger(__name__)
 
 
-class IndexCsvRenderer(csvbase.BaseCsvResponse):
-    columns = [_("Resource"), _("Count"), _("Cost")]
-
-    def get_row_data(self):
-        for b in self.context['current_month']:
-            yield (b.resource,
-                   b.count,
-                   defaultfilters.floatformat(b.cost, 2))
-
-
-class IndexView(horizon_tables.DataTableView):
-    table_class = tables.BillingTable
-    show_terminated = True
-    csv_template_name = 'management/billing/billing.csv'
+class IndexView(views.HorizonTemplateView):
     template_name = 'management/billing/index.html'
-    csv_response_class = IndexCsvRenderer
 
     def __init__(self, *args, **kwargs):
         super(IndexView, self).__init__(*args, **kwargs)
 
-    def get_template_names(self):
-        if self.request.GET.get('format', 'html') == 'csv':
-            return (self.csv_template_name or
-                    ".".join((self.template_name.rsplit('.', 1)[0], 'csv')))
-        return self.template_name
-
-    def get_content_type(self):
-        if self.request.GET.get('format', 'html') == 'csv':
-            return "text/csv"
-        return "text/html"
-
-    def get_data(self):
-        try:
-            project_id = self.kwargs.get('project_id',
-                                         self.request.user.tenant_id)
-            self.billing = base.BaseBilling(self.request, project_id)
-            self.start, self.end = self.billing.get_date_range()
-            distil_client = distil.distilclient(self.request)
-            self.history = (distil.get_cost(self.request, distil_client))
-
-            self.kwargs['billing'] = self.billing
-            self.kwargs['current_month'] = self.history[-1][1]
-            self.kwargs['history'] = self.history
-            return self.history[-1][1]
-        except Exception:
-            exceptions.handle(self.request, _('Unable to get usage cost.'))
-            return []
-
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['table'].kwargs['billing'] = self.billing
-        context['form'] = self.billing.form
-        context['billing'] = self.billing
-        context['current_month'] = self.history[-1][1]
-        pie_data = [{"value": b.cost, "key": b.resource}
-                    for b in self.history[-1][1] if b.cost >= 0]
-        line_data = [{"values": [{"y": m[0], "x": i}
-                                 for i, m in enumerate(self.history)],
-                      "method": "Square Root Choice", "key": "Cost"}]
-        chart_data = {'pie': pie_data, 'line': line_data}
-        context['chart_data'] = json.dumps(chart_data)
-        context['amount_cost'] = self.history[-1][0]
-        context['cost_details'] = json.dumps(self.history[-1][2])
+        distil_client = distil.distilclient(self.request)
+        self.cost = distil.get_cost(self.request, distil_client)
+        self.credits = distil.get_credits(self.request, distil_client)
+        pie_data = []
+        for i in range(len(self.cost)):
+            pie_data.append([{"value": value, "key": key} for (key, value)
+                             in self.cost[i]["breakdown"].items()])
+        avg_cost = round(sum([m["total_cost"]
+                              for m in self.cost[:11]]) / 11.0, 2)
+        line_data = [{"values": [{"y": m["total_cost"], "x": i,
+                                  "p": m.get("status")} for i, m
+                                 in enumerate(self.cost)], "key": "Cost"},
+                     {"values": [{"y": avg_cost, "x": i}
+                                 for i in range(12)],
+                      "key": "Avg Cost", "color": "#fdd0a2"}]
+
+        context['line_chart_data'] = json.dumps(line_data)
+        context['pie_chart_data'] = json.dumps(pie_data)
+        context['month_details'] = json.dumps([d["details"] for d
+                                               in self.cost])
         context['x_axis_line_chart'] = self._get_x_axis_for_line_chart()
+        context['credits'] = json.dumps(self.credits)
         return context
 
     def _get_x_axis_for_line_chart(self):
         today = datetime.date.today()
-        ordered_month = ['Jan ' + str(today.year), 'Feb', 'Mar', "Apr", 'May',
-                         'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        ordered_month = ['Jan ', 'Feb ', 'Mar ', "Apr ", 'May ', 'Jun ',
+                         'Jul ', 'Aug ', 'Sep ', 'Oct ', 'Nov ', 'Dec ']
 
-        return ordered_month[today.month:] + ordered_month[:today.month]
-
-    def render_to_response(self, context, **response_kwargs):
-        if self.request.GET.get('format', 'html') == 'csv':
-            render_class = self.csv_response_class
-            fn_template = "usage_cost_{0}_{1}.csv"
-            filename = fn_template.format(self.start.strftime('%Y-%m-%d'),
-                                          self.end.strftime('%Y-%m-%d'))
-            response_kwargs.setdefault("filename", filename)
-        else:
-            render_class = self.response_class
-        resp = render_class(request=self.request,
-                            template=self.get_template_names(),
-                            context=context,
-                            content_type=self.get_content_type(),
-                            **response_kwargs)
-        return resp
+        return ([m + str(today.year - 1) for m in ordered_month[today.month:]]
+                + [m + str(today.year) for m in ordered_month[:today.month]])
